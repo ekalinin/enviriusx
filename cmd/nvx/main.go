5502347b4900168c61d3e6235c4db896ae34e772
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-11-13 23:11:58 +0300

    added cmd module for main binary

---
+++ cmd/nvx/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ekalinin/enviriusx/commands"
+	_ "github.com/ekalinin/enviriusx/commands/loadall"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const version = "0.1.0"
+
+func main() {
+
+	app := kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version(version).Author("Eugene Kalinin")
+	kingpin.CommandLine.Help = "Universal Virtual Environments Manager v." + version
+
+	for _, mkCmd := range commands.Commands {
+		cmd := mkCmd()
+		cmd.Configure(app)
+	}
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+}
