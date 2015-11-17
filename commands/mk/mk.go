2aa49c4f0148c5e2124b222c8101a7c426ee7468
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-11-17 22:25:39 +0300

    added some logic into mk command

--- commands/mk/mk.go
+++ commands/mk/mk.go
@@ -1,26 +1,68 @@
 package mk
 
 import (
+	"errors"
+
 	"github.com/ekalinin/enviriusx/commands"
+	"github.com/ekalinin/enviriusx/env"
+	"github.com/ekalinin/enviriusx/langs"
+
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type MkCmd struct {
 	Name    string
 	EnvName string
+	Force   bool
+	AutoOn  bool
+	Langs   map[string]*string
+}
+
+func (cmd *MkCmd) generateEnvName() {
+	// TODO: implement
 }
 
 func (cmd *MkCmd) Run(c *kingpin.ParseContext) error {
+	// TODO: add  langs itno Env
+
+	if cmd.EnvName == "" {
+		cmd.generateEnvName()
+	}
+
+	newEnv := env.NewEnv(cmd.EnvName)
+
+	if newEnv.IsExists() {
+		if cmd.Force {
+			// TODO: add rm commend here with env name
+		} else {
+			// TODO: check that app error code != 0
+			return errors.New("Environment is already exists.")
+		}
+	}
+
+	newEnv.Create()
+
+	if cmd.AutoOn {
+		// TODO: activate environment
+	}
+
 	return nil
 }
 
 func (c *MkCmd) Configure(app *kingpin.Application) {
-	ls := app.Command(c.Name, "Create environment").Action(c.Run)
-	ls.Flag("env-name", "Environment name").Short('n').StringVar(&c.EnvName)
+	cmd := app.Command(c.Name, "Create environment").Action(c.Run)
+	cmd.Flag("env-name", "Environment name").Short('n').StringVar(&c.EnvName)
+	cmd.Flag("force", "Re-create environment if it already exists").Short('f').BoolVar(&c.Force)
+	cmd.Flag("on", "Activate environment after installation").BoolVar(&c.AutoOn)
+
+	for l, _ := range langs.Langs {
+		c.Langs[l] = new(string)
+		cmd.Flag(l, "Install certain version of the "+l).StringVar(c.Langs[l])
+	}
 }
 
 func init() {
-	cmd := MkCmd{"mk", ""}
+	cmd := MkCmd{"mk", "", false, false, map[string]*string{}}
 	commands.Add(cmd.Name, func() commands.Commander {
 		return &cmd
 	})
