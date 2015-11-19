48e4fd67c7d1c5b747eda39e2afd1492f670aba6
Eugene Kalinin <e.v.kalinin@gmail.com>
2015-11-19 22:34:53 +0300

    imp of the Env.IsExists

--- env/env.go
+++ env/env.go
@@ -1,6 +1,9 @@
 package env
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/ekalinin/enviriusx/langs"
 )
 
@@ -22,7 +25,14 @@ func (env *Environment) WriteMeta() {
 }
 
 func (env *Environment) IsExists() bool {
-	return true
+	_, err := os.Stat(filepath.Join(GetEnvHome(), env.name))
+	if err == nil {
+		return true //, nil
+	}
+	if os.IsNotExist(err) {
+		return false //, nil
+	}
+	return true //, err
 }
 
 func (env *Environment) Create() error {
