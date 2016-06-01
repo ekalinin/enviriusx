26b02c28cda3d40ab6d6b0c1bee4155d86438f91
Eugene Kalinin <e.v.kalinin@gmail.com>
2016-06-01 18:36:15 +0300

    nvx/main: prettified

--- cmd/nvx/main.go
+++ cmd/nvx/main.go
@@ -11,14 +11,18 @@ import (
 )
 
 func main() {
-	v := langs.Version
-	app := kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version(v).Author("Eugene Kalinin")
-	kingpin.CommandLine.Help = "Universal Virtual Environments Manager v" + v
+	app := kingpin.
+		UsageTemplate(kingpin.CompactUsageTemplate).
+		Version(langs.Version).
+		Author("Eugene Kalinin")
+
+	kingpin.CommandLine.Help = "Universal Virtual Environments Manager " +
+		"v." + langs.Version
 	kingpin.CommandLine.HelpFlag.Short('h')
 
 	for _, mkCmd := range commands.Commands {
-		cmd := mkCmd()
-		cmd.Configure(app)
+		mkCmd().Configure(app)
 	}
+
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
